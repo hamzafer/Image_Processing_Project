357943e985d45e8c84f44d1a95a93518bcdb2937
Hamza Zafar <hamzafer3@gmail.com>
2023-12-06 03:18:11 -0500

    minor text changes / figure changes - after submission

--- Week9/zigzagScan.m
+++ Week9/zigzagScan.m
@@ -14,9 +14,6 @@ out=zeros(1,num_rows*num_cols);
 
 cur_row=1;	cur_col=1;	cur_index=1;
 
-% First element
-%out(1)=in(1,1);
-
 while cur_row<=num_rows & cur_col<=num_cols
 	if cur_row==1 & mod(cur_row+cur_col,2)==0 & cur_col~=num_cols
 		out(cur_index)=in(cur_row,cur_col);
